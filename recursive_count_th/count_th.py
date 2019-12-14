c59880a4b0486c6ffb758e2f568b0169354d36b2
LD <LDinho@users.noreply.github.com>
2019-12-14 14:15:14 -0500

    add count soln

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -1,10 +1,23 @@
-'''
+"""
 Your function should take in a single parameter (a string `word`)
-Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
+Your function should return a count of how many occurrences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
-'''
+"""
+
+
 def count_th(word):
-    
     # TBC
-    
-    pass
+    substr = "th"
+
+    if len(word) < 2:
+        return 0
+    if substr == word[:2]:
+        # print(word[:2])
+        return count_th(word[1:]) + 1
+    else:
+        return count_th(word[1:])
+
+    # pass
+
+
+print(count_th("abcthfgth"))
